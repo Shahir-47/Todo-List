f979fb30047d01f9fa21ae1e8f978dabd42356a4
Shahir Ahmed <ahmeds@dickinson.edu>
2023-07-04 12:03:12 -0400

    made page List

--- src/pages/all.js
+++ src/pages/all.js
@@ -1,11 +1,17 @@
 const allUI = () => {
     const pageContent = document.querySelector('#page-content');
+    const footer = document.querySelector('footer');
 
     const addBtn = document.createElement('button');
     addBtn.classList.add('add-btn');
     addBtn.textContent = '+';
-
     pageContent.appendChild(addBtn);
+
+    const todoList = document.createElement('div');
+    todoList.classList.add('todo-list');
+    todoList.style.maxHeight = pageContent.offsetHeight - (footer.offsetHeight * 2) - 16 + 'px';
+    todoList.style.marginRight = addBtn.offsetWidth + 64 + 'px';
+    pageContent.appendChild(todoList);
 
 };
 
