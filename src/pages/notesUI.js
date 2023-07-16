dc80dd6a53fe03b50245bb243892a44b360c0425
Shahir Ahmed <ahmeds@dickinson.edu>
2023-07-16 03:36:29 -0400

    notesUI done

--- src/pages/notesUI.js
+++ src/pages/notesUI.js
@@ -1,4 +1,7 @@
 import { storage } from '../functions/storage';
+import del from '../assets/img/del.svg';
+import star from '../assets/img/star.svg';
+import Masonry from 'masonry-layout';
 
 const showAllNotes = (starred = false) => {
     const allSidebarItems = document.querySelectorAll('.item');
@@ -30,12 +33,107 @@ const showAllNotes = (starred = false) => {
 
     const todoList = document.createElement('div');
     todoList.classList.add('notes-list');
-    todoList.style.maxHeight = pageContent.offsetHeight - (footer.offsetHeight * 2.5) - (projectTitle.offsetHeight*3) - 16 + 'px';
+    todoList.style.maxHeight = pageContent.offsetHeight - (footer.offsetHeight*2) - (projectTitle.offsetHeight*1.5) - 16 + 'px';
     todoList.style.marginRight = addBtn.offsetWidth + 64 + 'px';
     projectTitle.style.marginRight = addBtn.offsetWidth + 72 + 'px';
     pageContent.appendChild(todoList);
 
-    // allProject(starred);
+    allNotes(starred);
 }
 
-export { showAllNotes }
+const allNotes = (starred) => {
+    const todoList = document.querySelector('.notes-list');
+    todoList.innerHTML = '';
+
+    // let notesList = storage.getNotes()[0].list;
+    let notesList = [
+        {
+            title: 'Note 1',
+            description: 'This is a note',
+            starred: false,
+            id: 1
+        },
+        {
+            title: 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',
+            description: 'This is a note',
+            starred: true,
+            id: 2
+        },
+        {
+            title: 'Note 3',
+            description: 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',
+            starred: true,
+            id: 3
+        },
+        {
+            title: 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',
+            description: 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',
+            starred: false,
+            id: 4
+        },
+        {
+            title: Array(72).fill('f').join(''),
+            description: Array(276).fill('f').join(''),
+            starred: false,
+            id: 5
+        },
+
+
+    ];
+    notesList.reverse(); // Reverse the order of the array
+    notesList.forEach((note, index) => {
+        const noteCard = document.createElement('div');
+        noteCard.classList.add('note');
+
+        const noteTitle = document.createElement('div');
+        noteTitle.classList.add('note-title');
+        noteTitle.contentEditable = true;
+        noteTitle.textContent = note.title;
+
+        const noteDescription = document.createElement('div');
+        noteDescription.classList.add('note-description');
+        noteDescription.contentEditable = true;
+        noteDescription.textContent = note.description;
+
+        const buttons = document.createElement('div');
+        buttons.classList.add('buttons');
+
+        const delBtn = document.createElement('button');
+        delBtn.classList.add('del-btn');
+        const delImg = document.createElement('img');
+        delImg.src = del;
+        delImg.alt = 'Delete Note';
+        delBtn.appendChild(delImg);
+
+        const starBtn = document.createElement('button');
+        starBtn.classList.add('star-btn');
+        const starImg = document.createElement('img');
+        starImg.src = star;
+        starImg.alt = 'Pin Note';
+        starBtn.appendChild(starImg);
+
+        buttons.appendChild(starBtn);
+        buttons.appendChild(delBtn);
+
+        noteCard.appendChild(noteTitle);
+        noteCard.appendChild(noteDescription);
+        noteCard.appendChild(buttons);
+
+        todoList.appendChild(noteCard);
+    });
+
+    // Initialize Masonry.js and specify the container element
+    let masonry = new Masonry(todoList, {
+        itemSelector: '.note',
+        columnWidth: '.note',
+        gutter: 10,
+    });
+
+    // Call masonry.layout() after adding the notes to the container
+    setTimeout(() => {
+        masonry.layout();
+    }, 0);
+
+}
+
+export { showAllNotes, allNotes }
